2495a75f58ca8dd77ce817ca10972fdc3e47fda8
Paulo Henrique <varelapaulo852@gmail.com>
2026-05-11 16:50:53 -0300

    Exception handling - delete

--- src/main/java/com/pauloh/course/services/UserService.java
+++ src/main/java/com/pauloh/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.pauloh.course.services;
 
 import com.pauloh.course.entities.User;
 import com.pauloh.course.repositories.UserRepository;
+import com.pauloh.course.services.services.exceptions.DatabaseException;
 import com.pauloh.course.services.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
